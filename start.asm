06d34babb314323ce96e10ed878e5bb3360a88e6
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2024-02-20 17:20:34 +0800

    Add files via upload

---
+++ start.asm
@@ -0,0 +1,18 @@
+START: 
+ LDA #66 
+ LDX #$A000 
+ STA ,X
+ LDA #105 
+ LDX #$A002 
+ STA ,X 
+ LDA #108 
+ LDX #$A004 
+ STA ,X
+ LDA #108 
+ LDX #$A006 
+ STA ,X
+ LDA #121 
+ LDX #$A008 
+ STA ,X 
+ END START
+	
